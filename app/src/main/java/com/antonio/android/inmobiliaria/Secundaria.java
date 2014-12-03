87a05a288fc0af3e9c8f26303068cf06a6f567ad
Antonio <antoniob.m.r@hotmail.es>
2014-12-03 18:36:57 +0100

    final

--- app/src/main/java/com/antonio/android/inmobiliaria/Secundaria.java
+++ app/src/main/java/com/antonio/android/inmobiliaria/Secundaria.java
@@ -117,9 +117,6 @@ public class Secundaria extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
